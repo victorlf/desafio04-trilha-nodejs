4544af65862c1d8c92367185e4a1249fd4d20d48
Victor Fonseca <victorlfonseca@gmail.com>
2021-07-08 01:53:43 -0300

    First commit, challenge finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const user_id = request.header("user_id");
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
